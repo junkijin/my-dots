b9c4fc3b2692cb7e23587db01ff64641a3a94ca3
Junki Jin <joonki@ab180.co>
2026-09-02 20:39:31 +0900

    Replace Codex wrapper with abbreviations

--- junkijin/.config/fish/config.fish
+++ junkijin/.config/fish/config.fish
@@ -3,6 +3,9 @@
 if status is-interactive
     set -gx VISUAL nvim
     set -gx SHELL (command -s fish)
+
+    abbr --add cx codex
+    abbr --add 'cx~' 'codex resume'
     abbr --add th treehouse
 end
 
