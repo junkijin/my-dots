def503edc2bbf5c183110829aea5180385db3d02
Junki Jin <jin@junki.dev>
2026-09-06 23:24:55 +0900

    Add tmux configuration and remove WezTerm setup

--- junkijin/.config/fish/config.fish
+++ junkijin/.config/fish/config.fish
@@ -1,9 +1,8 @@
 /opt/homebrew/bin/brew shellenv | source
 
 if status is-interactive
+    set -gx SHELL (status fish-path)
     set -gx VISUAL nvim
-    set -gx SHELL (command -s fish)
-
     abbr --add cx codex
     abbr --add 'cx~' 'codex resume'
     abbr --add th treehouse
